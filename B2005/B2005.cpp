7811fd2317bc98b538a83468a0a776a83b8462d1
ForsakenDelusion <wangcy0205@gmail.com>
2025-02-14 20:25:24 +0800

    first

---
+++ B2005/B2005.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char x;
+    std::cin >> x;
+    cout <<" "<<" "<<x<<endl;
+    cout <<" "<<x<<x<<x<<endl;
+    cout <<x<<x<<x<<x<<x<<endl;
+    return 0;
+}
